6912194573c02dddf0f477319196a6f25acf2a7e
Sebaxtian Gonzalez <manumartinez-wq>
2021-07-17 17:08:24 -0500

    taller pagina pais

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-http-client'`, () => {
+  it(`should have as title 'TALLER1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-http-client');
+    expect(app.title).toEqual('TALLER1');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-http-client!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TALLER1!');
   });
 });
